7bcf2c47df4bd04a6320ed8e3e2a73019dc7dccc
C-A-Y-D-E <notyetfamousprk@gmail.com>
2025-08-12 01:42:46 +0530

    k

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -44,9 +44,13 @@ const Hero = () => {
                 className="w-full max-lg:mx-auto max-w-[200px] md:max-w-[400px] lg:max-w-[500px] object-cover"
                 src="hero.png"
                 initial={{ scale: 0.8, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
+                animate={{ scale: 1, opacity: 1,transition:{
+                  duration: 1, delay: 0.6
+                } }}
                 transition={{ duration: 1, delay: 0.6 }}
-                whileHover={{ scale: 1.05 }}
+                whileHover={{ scale: 1.05,transition:{
+                  duration: 0.3,
+                }  }}
               />
               <motion.img
                 className="w-full max-w-[50px] md:max-w-[100px] lg:max-w-[200px] xl:max-w-[220px]  object-cover"
@@ -79,9 +83,12 @@ const Hero = () => {
               <motion.h1
                 className="text-5xl tracking-[5px] md:text-6xl lg:text-7xl xl:text-9xl uppercase font-rubikDoodle xl:tracking-[14px]"
                 initial={{ scale: 0.5, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
+                animate={{ scale: 1, opacity: 1}}
                 transition={{ duration: 1, delay: 1 }}
-                whileHover={{ scale: 1.05 }}
+                whileHover={{ scale: 1.05,transition:{
+                  duration: 0.3,
+           
+                }  }}
               >
                 Baby Troll
               </motion.h1>
@@ -89,11 +96,14 @@ const Hero = () => {
                 className="font-rubikBubbles tracking-widest text-lg uppercase md:text-xl lg:text-2xl py-3 px-10 lg:px-16 border-2 border-black shadow-[8px_8px_8px_0px_rgba(0,0,0,1)]"
                 href="/"
                 initial={{ y: 20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.6, delay: 1.2 }}
+                animate={{ y: 0, opacity: 1, transition: { duration: 0.6, delay: 1.2 } }}
+                transition={{ duration: 0.6, delay: 0 }}
                 whileHover={{
                   scale: 1.05,
                   boxShadow: "12px 12px 12px 0px rgba(0,0,0,1)",
+                  transition: {
+                    duration: 0.3,
+                  },
                 }}
                 whileTap={{ scale: 0.95 }}
               >
